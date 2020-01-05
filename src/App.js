c5bcee0d7c625319f15606726cf9edae239342f1
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-05 19:21:10 +0100

    addded places related comp.  and funct. of conditinally renderind based of userid from params

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from './user/pages/UsersPage';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/navigation/MainNavigation';
 
 const App = () => {
@@ -18,6 +19,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
