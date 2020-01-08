dbb3239acebb8737a847d6effaf5b74a9930e526
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-08 18:35:36 +0100

    finished input validation

--- src/shared/components/elements/formelements/input/Input.js
+++ src/shared/components/elements/formelements/input/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 /* useReducer kinda mini redux */
 import { validate } from '../../../../util/validators';
 
@@ -30,6 +30,13 @@ const Input = props => {
     isValid: false
   });
 
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = event => {
     dispatch({
       type: 'CHANGE',
@@ -51,10 +58,7 @@ const Input = props => {
         type={props.type}
         placeholder={props.placeholder}
         onChange={changeHandler}
-        onBlur={
-          touchHandler
-        } /* this help us to avoid 
-        problem of having false input before typing something to input */
+        onBlur={touchHandler}
         value={inputState.value}
       />
     ) : (
