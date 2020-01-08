dfbde6b4cea5ffcc74ec6f8b587ad767cb02cace
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-09 00:38:00 +0100

    finished all features functionality(in only react side)

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/elements/card/Card';
 import Input from '../../shared/components/elements/formelements/input/Input';
@@ -9,9 +9,12 @@ import {
   VALIDATOR_REQUIRE
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+
 import './Auth.css';
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -55,6 +58,7 @@ const Auth = () => {
   const authSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
+    auth.login();
   };
 
   return (
